09b09d6bfe0e102f09f648e91d3c6ee803935c30
thartbm <thartbm@gmail.com>
2022-11-16 16:49:53 -0500

    first stab at full set of descriptors for all participants

--- R/data.R
+++ R/data.R
@@ -1,12 +1,48 @@
 
 `%notin%` <- function(x,y) !(x %in% y)
 
+# ALL DESCRIPTORS -----
+
+getAllDescriptors <- function() {
+  
+  files <- read.csv('data/files.csv', stringsAsFactors = FALSE)
+  
+  participants <- files$participant[1:3]
+  
+  groups <- files$group[which(files$participant %in% participants)]
+  
+  descriptors <- do.call("rbind",lapply(participants,getParticipantDescriptors))
+  
+  descriptors <- cbind('group'=groups,
+                       'participant'=participants, 
+                       as.data.frame(descriptors)    )
+  
+  return(descriptors)
+  
+}
 
 # descriptors participant ----
 
 getParticipantDescriptors <- function(participant) {
   
+  files <- read.csv('data/files.csv', stringsAsFactors = FALSE)
+  
+  # get participant group:
+  idx   <- which(files$participant == participant)
+  group <- files$group[idx]
+  strat <- files$strategy_order[idx]
+  
+  cat(sprintf('working on: group %s, participant %s\n',group,participant))
+  
+  # determine which sessions are available:
+  sessions <- c('aligned',
+                'rotated')
+  if (substr(group,1,4) == 'org_') {
+    sessions <- c('aligned')
+  }
+  
   data <- prepareParticipantData(participant)
+  
   schedule <- getSchedule()
   
   descriptors <- list()
@@ -17,6 +53,50 @@ getParticipantDescriptors <- function(participant) {
   descriptors[['aligned_nocursor_sd']] <- getBaselineSD(schedule = schedule,
                                                         task = 'nocursor',
                                                         df = data[['aligned_nocursor_reachdeviations']])
+  
+  descriptors[['aligned_activelocalization_sd']] <- getLocalizationSD(data[['aligned_activelocalization']])
+  
+  descriptors[['aligned_passivelocalization_sd']] <- getLocalizationSD(data[['aligned_passivelocalization']])
+  
+  # # # # # # # # # # #
+  # NO ROTATED SESSION
+  
+  if ('rotated' %notin% sessions) {
+    descriptors[['training_rate']] <- NA
+    descriptors[['training_asymptote']] <- NA
+    descriptors[['rotated_activelocalization_sd']] <- NA
+    descriptors[['rotated_passivelocalization_sd']] <- NA
+    descriptors[['activelocalization_shift']] <- NA
+    descriptors[['passivelocalization_shift']] <- NA
+    descriptors[['inclusion']] <- NA
+    descriptors[['exclusion']] <- NA
+  }
+  
+  # # # # # # # # # # #
+  # YES ROTATED SESSION
+  
+  if (substr(group,1,10) == 'instructed') {
+    descriptors[['training_rate']] <- NA
+    descriptors[['training_asymptote']] <- NA
+  } else {
+    ADfit <- getADfit(df=data[['rotated_training_reachdeviations']])
+    descriptors[['training_rate']]      <- ADfit['lambda']
+    descriptors[['training_asymptote']] <- ADfit['N0']
+  }
+  
+  descriptors[['rotated_activelocalization_sd']] <- getLocalizationSD(data[['rotated_activelocalization']])
+  descriptors[['rotated_passivelocalization_sd']] <- getLocalizationSD(data[['rotated_passivelocalization']])
+  
+  descriptors[['activelocalization_shift']] <- getLocalizationShift(data[['rotated_activelocalization']])
+  descriptors[['passivelocalization_shift']] <- getLocalizationShift(data[['rotated_passivelocalization']])
+  
+  
+  RAE <- getReachAftereffects(df=data[['rotated_nocursor_reachdeviations']],
+                              strategy=strat,
+                              schedule=schedule)
+  
+  descriptors[['inclusion']] <- RAE['inclusion']
+  descriptors[['exclusion']] <- RAE['exclusion']
   
   return(descriptors)
   
@@ -53,34 +133,46 @@ prepareParticipantData <- function(participant) {
   # get aligned nocursor reach deviations
   data[['aligned_nocursor_reachdeviations']] <- getReachDeviationsMV(data[['aligned_nocursor']])
   
-  training_biases <- getBaselineBiases(schedule = schedule, 
+  # biases in training reaches:
+  data[['training_biases']] <- getBaselineBiases(schedule = schedule, 
                                        task = 'training', 
-                                       df = data[['aligned_training_reachdeviations']])
+                                       df   = data[['aligned_training_reachdeviations']])
   
-  nocursor_biases <- getBaselineBiases(schedule = schedule, 
+  # biases in no-cursor reaches:
+  data[['nocursor_biases']] <- getBaselineBiases(schedule = schedule, 
                                        task = 'nocursor', 
-                                       df = data[['aligned_nocursor_reachdeviations']])
-  
+                                       df   = data[['aligned_nocursor_reachdeviations']])
   
   # baseline training reaches:
   data[['aligned_training_reachdeviations']] <- baselineReaches(schedule=schedule,
                                                                 task='training',
                                                                 df=data[['aligned_training_reachdeviations']],
-                                                                biases=training_biases)
+                                                                biases=data[['training_biases']])
   # baseline nocursor reaches:
   data[['aligned_nocursor_reachdeviations']] <- baselineReaches(schedule=schedule,
                                                                 task='nocursor',
                                                                 df=data[['aligned_nocursor_reachdeviations']],
-                                                                biases=nocursor_biases)
+                                                                biases=data[['nocursor_biases']])
+  
+  # clean the aligned active localization data:
+  data[['aligned_activelocalization']] <- cleanLocalization(data[['aligned_activelocalization']])
+  
+  # clean the aligned passive localization data:
+  data[['aligned_passivelocalization']] <- cleanLocalization(data[['aligned_passivelocalization']])
+  
+  # get baseline active localization bias (a smooth spline fit object/list)
+  data[['activelocaization_bias']] <- getSpline(data[['aligned_activelocalization']])
+  
+  # get baseline passive localization bias (a smooth spline fit object/list)
+  data[['passivelocaization_bias']] <- getSpline(data[['aligned_passivelocalization']])
+  
+  data[['aligned_activelocalization']]  <- unbiasLocalization(locdf = data[['aligned_activelocalization']],
+                                                              bias  = data[['activelocaization_bias']])
+  #print(str(data[['aligned_activelocalizations']]))
+  data[['aligned_passivelocalization']] <- unbiasLocalization(locdf = data[['aligned_passivelocalization']],
+                                                              bias  = data[['passivelocaization_bias']])
   
   
-  # get baseline passive localization bias
-  
-  # get baseline active localization bias
-  
-  
-  
-
   # ROTATED
   
   if ('rotated' %notin% sessions) {
@@ -101,12 +193,24 @@ prepareParticipantData <- function(participant) {
   data[['rotated_training_reachdeviations']] <- baselineReaches(schedule=schedule,
                                                                 task='training',
                                                                 df=data[['rotated_training_reachdeviations']],
-                                                                biases=training_biases)
+                                                                biases=data[['training_biases']])
   # baseline nocursor reaches:
   data[['rotated_nocursor_reachdeviations']] <- baselineReaches(schedule=schedule,
                                                                 task='nocursor',
                                                                 df=data[['rotated_nocursor_reachdeviations']],
-                                                                biases=nocursor_biases)
+                                                                biases=data[['nocursor_biases']])
+  
+  
+  data[['rotated_activelocalization']] <- cleanLocalization(data[['rotated_activelocalization']])
+  
+  data[['rotated_passivelocalization']] <- cleanLocalization(data[['rotated_passivelocalization']])
+  
+  data[['rotated_activelocalization']] <- unbiasLocalization(locdf = data[['rotated_activelocalization']],
+                                                             bias  = data[['activelocaization_bias']])
+  
+  data[['rotated_passivelocalization']] <- unbiasLocalization(locdf = data[['rotated_passivelocalization']],
+                                                              bias  = data[['passivelocaization_bias']])
+  
   return(data)
   
 }
@@ -207,7 +311,7 @@ getBaselineBiases <- function(schedule, task, df) {
   
   df <- df[which(df$trial_num %in% trials),]
   
-  biases <- aggregate(reachdev_deg ~ targetangle_deg, data=df, FUN=median)
+  biases <- aggregate(reachdev_deg ~ targetangle_deg, data=df, FUN=median) # MEDIAN!?
   
   return(biases)
   
@@ -215,7 +319,7 @@ getBaselineBiases <- function(schedule, task, df) {
 
 # reach variance -----
 
-getBaselineSD <- function(schedule, task, df) {
+getBaselineSD <- function(schedule, task, df, useIQR=FALSE) {
   
   # identify the relevant trial numbers:
   aligned_trials <- schedule$trial_num[which(schedule$session=='aligned' & schedule$task==task)]
@@ -238,6 +342,160 @@ getBaselineSD <- function(schedule, task, df) {
   
   df <- df[which(df$trial_num %in% trials),]
   
-  return(sd(df$reachdev_deg))
+  reachdevs <- df$reachdev_deg
+  
+  # there are TRIMMED and WINSORIZED standard deviations,
+  # but they require scaling to be equivalent
+  # (unlike mean/median the value changes A LOT with extremes removed)
+  # unfortunately, the scaling math is complicated
+  # and NOT implemented in any R package afaik
+  
+  # so, to prevent errors, I will not do this
+  
+  # assuming a normal distribution, we could remove outliers
+  # using 1.5 times the interquartile range...
+  # code below
+  
+  # normal standard deviation:
+  std <- sd(reachdevs)
+  
+  # IQR "trimmed" sd
+  if (useIQR) {
+    qtls <- quantile(reachdevs, probs=c(0.25, 0.75), names=FALSE)
+    iqtr <- diff(qtls)
+    inlr <- qtls + (iqtr * c(-1*useIQR,1*useIQR))
+    std <- sd(reachdevs[which(reachdevs > inlr[1] & reachdevs < inlr[2])])
+  }
+
+  return(std)
+  
+}
+
+# learning curve -----
+
+getADfit <- function(df) {
+  
+  #print(str(df))
+  
+  df <- df[which(df$trial_num <= 90),]
+  
+  schedule <- rep(-1,90)
+  signal <- rep(NA,90)
+  
+  signal[df$trial_num] <- df$reachdev_deg
+  
+  fit <- Reach::asymptoticDecayFit(schedule = schedule,
+                                   signal =   signal)
+  
+  return(fit)
+  
+}
+
+# localization -----
+
+cleanLocalization <- function(df) {
+  
+  ntrials <- dim(df)[1]
+  
+  # remove hand angles too far from the arc?
+  idx <- which( df$targetangle_deg > (df$arcangle_deg - 45) &
+                df$targetangle_deg < (df$arcangle_deg + 45)    )
+  df <- df[idx,]
+  
+  # remove outliers based on tap errors?
+  df$tapangle_deg <- (atan2(df$tapy_cm, df$tapx_cm) / pi) * 180
+  df$taperror_deg <- df$tapangle_deg - df$targetangle_deg
+  
+  qtls <- quantile(df$taperror_deg, probs=c(0.25, 0.75), names=FALSE)
+  iqtr <- diff(qtls)
+  inlr <- qtls + (iqtr * c(-2,2))
+  idx <- which( df$taperror_deg > inlr[1] & 
+                df$taperror_deg < inlr[2]   )
+  df <- df[idx,]
+  
+  # further outlier removal?
+  # no.
+  
+  #cat(sprintf('removed %d trials\n',ntrials-dim(df)[1]))
+  
+  return(df)
+  
+}
+
+
+# this is essentially the BIAS in localization
+getSpline <- function(df) {
+  
+  X <- df$targetangle_deg
+  Y <- df$taperror_deg
+  
+  sm.spl <- smooth.spline(x=X,
+                          y=Y,
+                          spar=0.50,
+                          nknots=9)
+  
+  spl.fit <- sm.spl$fit
+  
+  return(spl.fit)
+  
+}
+
+unbiasLocalization <- function(locdf,
+                               bias) {
+  predicted_errors <- predict(bias,locdf$targetangle_deg)$y
+  
+  #print(predicted_errors)
+  
+  locdf$tapangle_deg <- locdf$tapangle_deg - predicted_errors
+  locdf$taperror_deg <- locdf$tapangle_deg - locdf$targetangle_deg
+  
+  return(locdf)
+  
+}
+
+getLocalizationSD <- function(locdf) {
+  
+  # assuming the localization bias has been removed
+  # we can just get the standard deviation of tap errors:
+  
+  return(sd(locdf$taperror_deg))
+  
+}
+
+getLocalizationShift <- function(locdf) {
+  
+  # assuming the localization bias has been removed
+  # we can simply get the median tap error:
+  
+  return(median(locdf$taperror_deg))
+  
+}
+
+# reach aftereffects -----
+
+getReachAftereffects <- function(df,
+                                 strategy,
+                                 schedule) {
+  
+  schedule <- schedule[which(schedule$session == 'rotated'),]
+  
+  if (strategy == "") {
+    return(c('inclusion'=NA, 'exclusion'=NA))
+  }
+  
+  if (strategy == "EI") {
+    excl.trn <- schedule$trial_num[which(schedule$strategy == 0)]
+    incl.trn <- schedule$trial_num[which(schedule$strategy == 1)]
+  }
+  if (strategy == "IE") {
+    excl.trn <- schedule$trial_num[which(schedule$strategy == 1)]
+    incl.trn <- schedule$trial_num[which(schedule$strategy == 0)]
+  }
+  
+  inclusion <- mean(df$reachdev_deg[which(df$trial_num %in% incl.trn)])
+  exclusion <- mean(df$reachdev_deg[which(df$trial_num %in% excl.trn)])
+  
+  return(c('inclusion'=inclusion,
+           'exclusion'=exclusion))
   
 }
