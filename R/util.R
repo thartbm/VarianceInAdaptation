741fd9078d153996efbf7f6f5301a788e1f7a3b7
thartbm <thartbm@gmail.com>
2022-11-20 09:54:50 -0500

    add %notin% operator, add subtask numbers to schedule, commented out earlier functions for decsriptors (should remove entirely at some point)

--- R/util.R
+++ R/util.R
@@ -1,3 +1,6 @@
+
+`%notin%` <- function(x,y) !(x %in% y)
+
 # data download code -----
 
 #' @title Download project data from OSF repository.
@@ -85,7 +88,7 @@ getDownloadList <- function(groups='all', sections=c('aligned','rotated')) {
 }
 
 
-# data processing -----
+# data organizing -----
 
 addSubTaskNumber <- function(df, session) {
   
@@ -122,160 +125,160 @@ addSubTaskNumber <- function(df, session) {
   
 }
 
-getAdaptationParameters <- function(groups=c('control',
-                                             'cursorjump',
-                                             'handview',
-                                             'older_control',
-                                             'EDSmatch')) {
-  
-  files <- read.csv('data/files.csv', stringsAsFactors = FALSE)
-  files <- files[which(files$group %in% groups),]
-  
-  N <- dim(files)[1]
-  
-  participant <- c()
-  group       <- c()
-  lambda      <- c()
-  N0          <- c()
-  
-  for (rown in c(1:dim(files)[1])) {
-    cat(sprintf('participant %d/%d\n',rown, N))
-    
-    # get relevant information from the files data frame:
-    pp_name <- files$participant[rown]
-    group_name <- files$group[rown]
-    baseline_filename <- files$aligned_training[rown]
-    adaptation_filename <- files$rotated_training[rown]
-    
-    baseline_trials <- getBaselineTrials(group_name, pp_name, baseline_filename)
+# getAdaptationParameters <- function(groups=c('control',
+#                                              'cursorjump',
+#                                              'handview',
+#                                              'older_control',
+#                                              'EDSmatch')) {
+#   
+#   files <- read.csv('data/files.csv', stringsAsFactors = FALSE)
+#   files <- files[which(files$group %in% groups),]
+#   
+#   N <- dim(files)[1]
+#   
+#   participant <- c()
+#   group       <- c()
+#   lambda      <- c()
+#   N0          <- c()
+#   
+#   for (rown in c(1:dim(files)[1])) {
+#     cat(sprintf('participant %d/%d\n',rown, N))
+#     
+#     # get relevant information from the files data frame:
+#     pp_name <- files$participant[rown]
+#     group_name <- files$group[rown]
+#     baseline_filename <- files$aligned_training[rown]
+#     adaptation_filename <- files$rotated_training[rown]
+#     
+#     baseline_trials <- getBaselineTrials(group_name, pp_name, baseline_filename)
+# 
+#     # get the biases for each target:
+#     biases <- aggregate(reachdev_deg ~ targetangle_deg, data=baseline_trials, FUN=median, na.rm=TRUE)
+#     
+#     # load rotated training data:
+#     learning_file <- read.csv(sprintf('data/%s/%s/%s',group_name, pp_name, adaptation_filename), stringsAsFactors = FALSE)
+#     # only keep data from:
+#     # - the first 90 rotated trials
+#     # - selected trials
+#     # - max velocity samples
+#     learning_trials <- learning_file[which( learning_file$trial_num <= 90 &
+#                                             learning_file$trialselected == 1 &
+#                                             learning_file$maxvelocity == 1),]
+#     # get reach deviations at max velocity for these 90 trials:
+#     learning_trials$reachdev_deg <- ((atan2(learning_trials$handy_cm, learning_trials$handx_cm) / pi) * 180) - learning_trials$targetangle_deg
+#     
+#     # subtract baseline bias from each reach deviation:
+#     for (target in biases$targetangle_deg) {
+#       bias <- biases$reachdev_deg[which(biases$targetangle_deg == target)]
+#       idx <- which(learning_trials$targetangle_deg == target)
+#       learning_trials$reachdev_deg[idx] <- learning_trials$reachdev_deg[idx] - bias
+#     }
+#     
+#     # remove reach deviations that are 30 degrees or more out of range:
+#     learning_trials <- learning_trials[which(learning_trials$reachdev_deg <  60 &
+#                                              learning_trials$reachdev_deg > -30   ),]
+#     
+#     # get a learning curve:
+#     reaches <- rep(NA, 90)
+#     reaches[learning_trials$trial_num] <- learning_trials$reachdev_deg
+#     
+#     # remove participants who didn't adapt or overadapted:
+#     asymptote <- mean(reaches[61:90], na.rm=TRUE)
+#     if ((asymptote < 15) | (asymptote > 45)) {
+#       next()
+#     }
+#     
+#     ADfit <- Reach::asymptoticDecayFit(schedule=rep(-1,90),
+#                                         signal=reaches)
+#     
+#     if ((ADfit['N0'] < 15) | (ADfit['N0'] > 45)) {
+#       next()
+#     }
+#     
+#     # add to vectors:
+#     participant <- c(participant, pp_name)
+#     group       <- c(group, group_name)
+#     lambda      <- c(lambda, ADfit['lambda'])
+#     N0          <- c(N0, ADfit['N0'])
+#     
+#   }
+#   
+#   df <- data.frame(group, participant, lambda, N0)
+#   write.csv(df, 'data/adaptation_parameters.csv', row.names=FALSE, quote=FALSE)
+#   return(df)
+#   
+# }
 
-    # get the biases for each target:
-    biases <- aggregate(reachdev_deg ~ targetangle_deg, data=baseline_trials, FUN=median, na.rm=TRUE)
-    
-    # load rotated training data:
-    learning_file <- read.csv(sprintf('data/%s/%s/%s',group_name, pp_name, adaptation_filename), stringsAsFactors = FALSE)
-    # only keep data from:
-    # - the first 90 rotated trials
-    # - selected trials
-    # - max velocity samples
-    learning_trials <- learning_file[which( learning_file$trial_num <= 90 &
-                                            learning_file$trialselected == 1 &
-                                            learning_file$maxvelocity == 1),]
-    # get reach deviations at max velocity for these 90 trials:
-    learning_trials$reachdev_deg <- ((atan2(learning_trials$handy_cm, learning_trials$handx_cm) / pi) * 180) - learning_trials$targetangle_deg
-    
-    # subtract baseline bias from each reach deviation:
-    for (target in biases$targetangle_deg) {
-      bias <- biases$reachdev_deg[which(biases$targetangle_deg == target)]
-      idx <- which(learning_trials$targetangle_deg == target)
-      learning_trials$reachdev_deg[idx] <- learning_trials$reachdev_deg[idx] - bias
-    }
-    
-    # remove reach deviations that are 30 degrees or more out of range:
-    learning_trials <- learning_trials[which(learning_trials$reachdev_deg <  60 &
-                                             learning_trials$reachdev_deg > -30   ),]
-    
-    # get a learning curve:
-    reaches <- rep(NA, 90)
-    reaches[learning_trials$trial_num] <- learning_trials$reachdev_deg
-    
-    # remove participants who didn't adapt or overadapted:
-    asymptote <- mean(reaches[61:90], na.rm=TRUE)
-    if ((asymptote < 15) | (asymptote > 45)) {
-      next()
-    }
-    
-    ADfit <- Reach::asymptoticDecayFit(schedule=rep(-1,90),
-                                        signal=reaches)
-    
-    if ((ADfit['N0'] < 15) | (ADfit['N0'] > 45)) {
-      next()
-    }
-    
-    # add to vectors:
-    participant <- c(participant, pp_name)
-    group       <- c(group, group_name)
-    lambda      <- c(lambda, ADfit['lambda'])
-    N0          <- c(N0, ADfit['N0'])
-    
-  }
-  
-  df <- data.frame(group, participant, lambda, N0)
-  write.csv(df, 'data/adaptation_parameters.csv', row.names=FALSE, quote=FALSE)
-  return(df)
-  
-}
-
-getBaselineTrials <- function(group_name, pp_name, baseline_filename) {
-  
-  # load the aligned training file:
-  baseline_file <- read.csv(sprintf('data/%s/%s/%s',group_name, pp_name, baseline_filename), stringsAsFactors = FALSE)
-  # only keep the relevant samples:
-  # - selected trials
-  # - maximum velocity
-  baseline_file <- baseline_file[which(   baseline_file$trialselected == 1 &
-                                            baseline_file$maxvelocity == 1),]
-  # for the nest step, we need to know which subtask each trial belonged to:
-  baseline_file <- addSubTaskNumber(baseline_file, session='aligned')
-  # keep only trials 7,8,9 from the "top-up" training trials
-  # and the last 9 trials from the first aligned training task
-  baseline_trials <- rbind(baseline_file[which(baseline_file$subtask > 2 & baseline_file$trial_num > 3),],
-                           baseline_file[which(baseline_file$subtask == 1 & baseline_file$trial_num > 33),])
-  # calculate the reach deviation in degrees angle
-  baseline_trials$reachdev_deg <- ((atan2(baseline_trials$handy_cm, baseline_trials$handx_cm) / pi) * 180) - baseline_trials$targetangle_deg
-  
-  return(baseline_trials)
-   
-}
-
-getBaselineDescriptors <- function(group_name, pp_name, baseline_filename) {
-  
-  # load the aligned training file:
-  baseline_file <- read.csv(sprintf('data/%s/%s/%s',group_name, pp_name, baseline_filename), stringsAsFactors = FALSE)
-  # only keep relevant trials:
-  baseline_file <- rbind(baseline_file[which(baseline_file$subtask > 2 & baseline_file$trial_num > 3),],
-                         baseline_file[which(baseline_file$subtask == 1 & baseline_file$trial_num > 33),])
-  baseline_file <- baseline_file[which(baseline_file$trialselected == 1),]
-  # for the nest step, we need to know which subtask each trial belonged to:
-  baseline_file <- addSubTaskNumber(baseline_file, session='aligned')
-  
-  
-  # trajectory length
-  # target is at 12 cm
-  average_path_length
-  sd_path_length
-  
-  # trajectory duration
-  average_path_duration
-  sd_path_duration
-  
-  # maximum velocity:
-  average_max_velocity
-  sd_max_velocity
-  
-  # max velocity time:
-  average_maxvel_time
-  sd_maxvel_time
-  
-  # max velocity point:
-  average_maxvel_distance
-  sd_maxvel_distance
-  
-  
-  # only keep the relevant samples:
-  # - selected trials
-  # - maximum velocity
-  baseline_file <- baseline_file[which(   baseline_file$trialselected == 1 &
-                                            baseline_file$maxvelocity == 1),]
-
-  # calculate the reach deviation in degrees angle
-  baseline_trials$reachdev_deg <- ((atan2(baseline_trials$handy_cm, baseline_trials$handx_cm) / pi) * 180) - baseline_trials$targetangle_deg
-  
-  # reach bias at maximum velocity:
-  average_bias
-  sd_bias
-  
-}
+# getBaselineTrials <- function(group_name, pp_name, baseline_filename) {
+#   
+#   # load the aligned training file:
+#   baseline_file <- read.csv(sprintf('data/%s/%s/%s',group_name, pp_name, baseline_filename), stringsAsFactors = FALSE)
+#   # only keep the relevant samples:
+#   # - selected trials
+#   # - maximum velocity
+#   baseline_file <- baseline_file[which(   baseline_file$trialselected == 1 &
+#                                             baseline_file$maxvelocity == 1),]
+#   # for the nest step, we need to know which subtask each trial belonged to:
+#   baseline_file <- addSubTaskNumber(baseline_file, session='aligned')
+#   # keep only trials 7,8,9 from the "top-up" training trials
+#   # and the last 9 trials from the first aligned training task
+#   baseline_trials <- rbind(baseline_file[which(baseline_file$subtask > 2 & baseline_file$trial_num > 3),],
+#                            baseline_file[which(baseline_file$subtask == 1 & baseline_file$trial_num > 33),])
+#   # calculate the reach deviation in degrees angle
+#   baseline_trials$reachdev_deg <- ((atan2(baseline_trials$handy_cm, baseline_trials$handx_cm) / pi) * 180) - baseline_trials$targetangle_deg
+#   
+#   return(baseline_trials)
+#    
+# }
+# 
+# getBaselineDescriptors <- function(group_name, pp_name, baseline_filename) {
+#   
+#   # load the aligned training file:
+#   baseline_file <- read.csv(sprintf('data/%s/%s/%s',group_name, pp_name, baseline_filename), stringsAsFactors = FALSE)
+#   # only keep relevant trials:
+#   baseline_file <- rbind(baseline_file[which(baseline_file$subtask > 2 & baseline_file$trial_num > 3),],
+#                          baseline_file[which(baseline_file$subtask == 1 & baseline_file$trial_num > 33),])
+#   baseline_file <- baseline_file[which(baseline_file$trialselected == 1),]
+#   # for the nest step, we need to know which subtask each trial belonged to:
+#   baseline_file <- addSubTaskNumber(baseline_file, session='aligned')
+#   
+#   
+#   # trajectory length
+#   # target is at 12 cm
+#   average_path_length
+#   sd_path_length
+#   
+#   # trajectory duration
+#   average_path_duration
+#   sd_path_duration
+#   
+#   # maximum velocity:
+#   average_max_velocity
+#   sd_max_velocity
+#   
+#   # max velocity time:
+#   average_maxvel_time
+#   sd_maxvel_time
+#   
+#   # max velocity point:
+#   average_maxvel_distance
+#   sd_maxvel_distance
+#   
+#   
+#   # only keep the relevant samples:
+#   # - selected trials
+#   # - maximum velocity
+#   baseline_file <- baseline_file[which(   baseline_file$trialselected == 1 &
+#                                             baseline_file$maxvelocity == 1),]
+# 
+#   # calculate the reach deviation in degrees angle
+#   baseline_trials$reachdev_deg <- ((atan2(baseline_trials$handy_cm, baseline_trials$handx_cm) / pi) * 180) - baseline_trials$targetangle_deg
+#   
+#   # reach bias at maximum velocity:
+#   average_bias
+#   sd_bias
+#   
+# }
 
 dataChecks <- function( groups   = c('control'), 
                         sessions = c('aligned'), 
@@ -426,12 +429,26 @@ getSchedule <- function() {
     
   }
   
-  return( data.frame(
-                      trial_num,
-                      session,
-                      task,
-                      strategy,
-                      repetition
-                     ) ) 
+  # create data frame:
+  schedule <- data.frame(
+                trial_num,
+                session,
+                task,
+                strategy,
+                repetition
+              )
+  
+  # split into aligned & rotated data frames:
+  aligned_schedule <- schedule[which(schedule$session == 'aligned'),]
+  rotated_schedule <- schedule[which(schedule$session == 'rotated'),]
+  
+  # add subtask numbers for later convenience:
+  aligned_schedule <- addSubTaskNumber(aligned_schedule, 'aligned')
+  rotated_schedule <- addSubTaskNumber(rotated_schedule, 'rotated')
+  
+  # put them back together:
+  schedule <- rbind(aligned_schedule, rotated_schedule)
+  
+  return( schedule ) 
   
 }
