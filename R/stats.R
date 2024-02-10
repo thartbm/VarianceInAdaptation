13dd7d6ac8de5728c215ffc27e43167e74abf8a6
Marius 't Hart <thartbm@gmail.com>
2024-02-10 12:48:46 -0500

    wrote function to extra data frames for stats, added first stats function

--- R/stats.R
+++ R/stats.R
@@ -4,6 +4,84 @@ library(reshape2)
 library(afex)
 library(BayesFactor)
 
+
+getDF4stats <- function(groups='all',variables=c('aligned_training_sd', 'aligned_nocursor_sd')) {
+  
+  df <- read.csv('data/descriptors.csv', stringsAsFactors = FALSE)
+  
+  # check groups for sanity:
+  datagroups <- unique(df$group)
+  
+  if (is.character(groups) & length(groups) == 1) {
+    if (groups == 'all') {
+      select_groups <- datagroups
+    } else if (groups %in% datagroups) {
+      select_groups <- c(groups)
+    }
+  } else if (length(groups) > 1) {
+    select_groups <- intersect(groups, datagroups)
+    if (length(select_groups) != length(groups)) {
+      cat('some groups not found in data, skipping:\n')
+      print(setdiff(groups, datagroups))
+    }
+  }
+  
+  df <- df[which(df$group %in% select_groups),]
+  
+  
+  # check variables for sanity:
+  datavariables <- names(df)[3:dim(df)[2]]
+  
+  
+  if (is.character(variables) & length(variables) == 1) {
+    if (variables == 'all') {
+      select_variables <- datavariables
+    } else if (variables %in% datavariables) {
+      select_variables <- c(variables)
+    }
+  } else if (length(variables) > 1) {
+    select_variables <- intersect(variables, datavariables)
+    if (length(select_variables) != length(variables)) {
+      cat('some groups not found in data, skipping:\n')
+      print(setdiff(variables, datavariables))
+    }
+  }
+  
+  df <- reshape2::melt(df[,c('group','participant',select_variables)],
+                       id.vars=c('group','participant'))
+  
+  return(df)
+  
+}
+
+baselineVarianceTests <- function() {
+  
+  df      <- getDF4stats(groups    = 'all',
+                         variables = c('aligned_training_sd',
+                                       'aligned_nocursor_sd',
+                                       'aligned_activelocalization_sd',
+                                       'aligned_passivelocalization_sd'))
+  
+  df$age <- 'younger'
+  df$age[which(df$group %in% c('older_control', 'older_instructed'))] <- 'older'
+  
+  df$age <- as.factor(df$age)
+  df$variable <- as.factor(df$variable)
+  
+  baymod <- BayesFactor::anovaBF(value ~ variable + age, data=df)
+  # bm_e   <- BayesFactor::extractBF( baymod )
+  
+  # cat('main effects:\n')
+  # print(baymod[c(1:2)])
+  # cat('interaction:\n')
+  # print(bm_e$bf[4] / bm_e$bf[3])
+  
+  print(bayestestR::bayesfactor_inclusion(baymod))
+  
+  
+}
+
+
 stats4_sd_distributions <- function() {
   
   all_df <- read.csv('data/descriptors.csv', stringsAsFactors = FALSE)
